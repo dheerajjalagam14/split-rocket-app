3ae8a80871f0c1ea4dd6bde11b1063f32bce4914
dheeraj <dheerajjalagam@gmail.com>
2022-09-26 09:25:47 -0500

    add service

--- split-rocket-ui/src/app/app.service.ts
+++ split-rocket-ui/src/app/app.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  rootURL = '/api';
+
+  calculateExpenses(data: any) {
+    return this.http.post(this.rootURL + '/calculate', data);
+  }
+
 }
