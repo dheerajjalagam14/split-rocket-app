215272bea5869c9de067851b131d006435825cdc
dheeraj <dheerajjalagam@gmail.com>
2022-09-25 23:48:08 -0500

    setup dist build for node js server

--- index.js
+++ index.js
@@ -3,23 +3,14 @@ const app = express(),
       bodyParser = require("body-parser");
       port = 3000;
 
-const users = [];
-
 app.use(bodyParser.json());
+app.use(express.static(process.cwd()+"/split-rocket-ui/dist/split-rocket-ui/"));
 
-app.get('/api/users', (req, res) => {
-  res.json(users);
-});
-
-app.post('/api/user', (req, res) => {
-  const user = req.body.user;
-  users.push(user);
-  res.json("user addedd");
-});
 
 app.get('/', (req,res) => {
-    res.send('App Works !!!!');
+  res.sendFile(process.cwd()+"/split-rocket-ui/dist/split-rocket-ui/index.html")
 });
+
 
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
